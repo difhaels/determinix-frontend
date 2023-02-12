a7b7f9a5148134372a47e101bf546277d424838c
Agung Saputra <agungsaputradifh@gmail.com>
2023-02-12 22:46:52 +0700

    added text home

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,15 +25,27 @@ export default function Home() {
         spaceBetween={50}
         slidesPerView={1}
         pagination={{ clickable: true, backgroundColor: "#000" }}
-        autoplay={{ delay: 4500 }}
+        autoplay={{ delay: 2500 }}
       >
         <SwiperSlide>
+          <div className="absolute top-[30%] left-[23%] text-center">
+            <h1 className="text-[120px]  text-[#ff0000] font-bold shadow-xl">DETERMINIX</h1>
+            <h2 className="text-[40px]  text-[#ffff] font-bold shadow-xl">A team working in the field of web developers</h2>
+          </div>
           <img src={home1} width="100%" />
         </SwiperSlide>
         <SwiperSlide>
+          <div className="absolute top-[30%] left-[23%] text-center">
+            <h1 className="text-[120px]  text-green-400 font-bold shadow-xl">LIFE.</h1>
+            <h2 className="text-[40px]  text-[#ffff] font-bold shadow-xl">We try to make the web as alive as possible</h2>
+          </div>
           <img src={home2} width="100%" />
         </SwiperSlide>
         <SwiperSlide>
+          <div className="absolute top-[30%] left-[23%] text-center">
+            <h1 className="text-[120px]  text-teal-500 font-bold shadow-sm">COOL.</h1>
+            <h2 className="text-[40px]  text-cyan-500 font-bold ">It's an obligation for us to make something cool</h2>
+          </div>
           <img src={home3} width="100%" />
         </SwiperSlide>
       </Swiper>
