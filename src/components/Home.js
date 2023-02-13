9b52cd69c1bc78a3ee5b6248f5843bd98267f4ee
Agung Saputra <agungsaputradifh@gmail.com>
2023-02-13 21:01:34 +0700

    make responsive again v1

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,37 +18,51 @@ import "./css/Home.css";
 
 export default function Home() {
   return (
-    <div className="bg-black">
-      <Swiper
-        // install Swiper modules
-        modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={1}
-        pagination={{ clickable: true, backgroundColor: "#000" }}
-        autoplay={{ delay: 5000 }}
-      >
-        <SwiperSlide>
-          <div className="absolute left-0 right-0 top-52 3xl:top-72 text-center">
-            <h1 className="text-[120px] text-[#ff0000] font-bold shadow-xl">DETERMINIX</h1>
-            <h2 className="text-[40px]  text-[#ffff] bg-[#1D1D1D] font-bold shadow-xl mx-72 3xl:mx-[450px]">A team working in the field of web developers</h2>
-          </div>
-          <img src={home1} width="100%" alt="home1" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="absolute left-0 right-0 top-52 3xl:top-72 text-center">
-            <h1 className="text-[120px]  text-green-400 font-bold shadow-xl">LIFE.</h1>
-            <h2 className="text-[40px]  text-[#ffff] bg-[#1D1D1D] font-bold shadow-xl mx-60 3xl:mx-[400px]">Determinix try to make the web as alive as possible</h2>
-          </div>
-          <img src={home2} width="100%" alt="home2" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="absolute left-0 right-0 top-52 3xl:top-72 text-center">
-            <h1 className="text-[120px]  text-teal-500 font-bold shadow-sm">COOL.</h1>
-            <h2 className="text-[40px]  text-[#ffff] bg-[#1D1D1D] font-bold py-1 mx-72 3xl:mx-[400px]">It's an obligation for Dx to make something cool</h2>
-          </div>
-          <img src={home3} width="100%" alt="home3" />
-        </SwiperSlide>
-      </Swiper>
+    <div className="">
+      <div className="bg-[#292828] block sm:hidden">
+        <h1 className=" py-96 text-[#ff0000] text-5xl font-bold text-center">DETERMINIX</h1>
+        <div className="text-center text-3xl px-3 text-white pb-36">
+          <h1 className="text-xl font-bold">INTRO</h1>
+          <h1 className="text-[#ff0000] font-bold text-left pt-20">DETERMINIX</h1>
+          <h1 className="text-left">A team working in the field of web developers.</h1>
+          <h1 className="text-green-400 font-bold text-right pt-20">LIFE.</h1>
+          <h1 className="text-right">Determinix try to make the web as alive as possible</h1>
+          <h1 className="text-teal-500 font-bold text-left pt-20">COOL.</h1>
+          <h1 className="text-left">It's an obligation for Dx to make something cool</h1>
+        </div>
+      </div>
+      <div className="bg-[#131313] hidden sm:block">
+        <Swiper
+          // install Swiper modules
+          modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={1}
+          pagination={{ clickable: true, backgroundColor: "#000" }}
+          autoplay={{ delay: 5000 }}
+        >
+          <SwiperSlide>
+            <div className="absolute left-0 right-0 top-52 3xl:top-72 text-center">
+              <h1 className="text-[120px] text-[#ff0000] font-bold shadow-xl">DETERMINIX</h1>
+              <h2 className="text-[40px]  text-[#ffff] bg-[#1D1D1D] font-bold shadow-xl mx-72 3xl:mx-[450px]">A team working in the field of web developers</h2>
+            </div>
+            <img src={home1} width="100%" alt="home1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="absolute left-0 right-0 top-52 3xl:top-72 text-center">
+              <h1 className="text-[120px]  text-green-400 font-bold shadow-xl">LIFE.</h1>
+              <h2 className="text-[40px]  text-[#ffff] bg-[#1D1D1D] font-bold shadow-xl mx-60 3xl:mx-[400px]">Determinix try to make the web as alive as possible</h2>
+            </div>
+            <img src={home2} width="100%" alt="home2" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="absolute left-0 right-0 top-52 3xl:top-72 text-center">
+              <h1 className="text-[120px]  text-teal-500 font-bold shadow-sm">COOL.</h1>
+              <h2 className="text-[40px]  text-[#ffff] bg-[#1D1D1D] font-bold py-1 mx-72 3xl:mx-[400px]">It's an obligation for Dx to make something cool</h2>
+            </div>
+            <img src={home3} width="100%" alt="home3" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
