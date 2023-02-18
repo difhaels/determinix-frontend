191ce650f5480778508e5f1e96c068ed922d2e79
Agung Saputra <agungsaputradifh@gmail.com>
2023-02-18 16:57:54 +0700

    change aos delay at homeMobile

--- src/components/Home-mobile.js
+++ src/components/Home-mobile.js
@@ -23,7 +23,7 @@ const Intro = () => {
         INTRO
         <div className="mx-auto w-[15%] h-[2.5px] bg-white mt-1 mb-5"></div>
       </h1>
-      <div data-aos="fade-up" data-aos-delay="400" className="px-3 py-4 border-2 border-white bg-red-500">
+      <div data-aos="fade-up" data-aos-delay="300" className="px-3 py-4 border-2 border-white bg-red-500">
         <h1 className="text-slate-900 font-bold ">DETERMINIX</h1>
         <h1 className="text-xl">A team working in the field of web developers.</h1>
       </div>
@@ -31,7 +31,7 @@ const Intro = () => {
         <h1 className="text-green-400 font-bold">LIFE.</h1>
         <h1 className="text-xl">Determinix try to make the web as alive as possible.</h1>
       </div>
-      <div data-aos="fade-up" data-aos-delay="400" className="px-3 py-4 border-2 border-white bg-red-500">
+      <div data-aos="fade-up" data-aos-delay="500" className="px-3 py-4 border-2 border-white bg-red-500">
         <h1 className="text-teal-500 font-bold ">COOL.</h1>
         <h1 className="text-xl">It's an obligation for Dx to make something cool.</h1>
       </div>
