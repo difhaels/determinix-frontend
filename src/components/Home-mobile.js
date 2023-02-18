26621323010bd03cffd306d676636fedc4ca4480
Agung Saputra <agungsaputradifh@gmail.com>
2023-02-18 16:41:14 +0700

    added aos at homeMobile

--- src/components/Home-mobile.js
+++ src/components/Home-mobile.js
@@ -1,3 +1,7 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const Home = () => {
   return (
     <div className="h-[100vh] text-center">
@@ -10,21 +14,24 @@ const Home = () => {
 };
 
 const Intro = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div className="text-center text-3xl px-9 text-white grid gap-5 pb-32">
       <h1 className="text-xl font-bold">
         INTRO
         <div className="mx-auto w-[15%] h-[2.5px] bg-white mt-1 mb-5"></div>
       </h1>
-      <div className="px-3 py-4 border-2 border-white bg-red-500">
+      <div data-aos="fade-up" data-aos-delay="400" className="px-3 py-4 border-2 border-white bg-red-500">
         <h1 className="text-slate-900 font-bold ">DETERMINIX</h1>
         <h1 className="text-xl">A team working in the field of web developers.</h1>
       </div>
-      <div className="px-3 py-4 border-2 border-white bg-red-500">
+      <div data-aos="fade-up" data-aos-delay="400" className="px-3 py-4 border-2 border-white bg-red-500">
         <h1 className="text-green-400 font-bold">LIFE.</h1>
         <h1 className="text-xl">Determinix try to make the web as alive as possible.</h1>
       </div>
-      <div className="px-3 py-4 border-2 border-white bg-red-500">
+      <div data-aos="fade-up" data-aos-delay="400" className="px-3 py-4 border-2 border-white bg-red-500">
         <h1 className="text-teal-500 font-bold ">COOL.</h1>
         <h1 className="text-xl">It's an obligation for Dx to make something cool.</h1>
       </div>
