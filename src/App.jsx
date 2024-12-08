bd65592f0dc7b615dbe980d6473603b71ed8b5ab
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-08 22:49:26 +0700

    tw

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Mantap</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../css/output.css"
+
+export default function home() {
+  return (
+    <div className='bg-blue-600 text-yellow-300'>home</div>
+  )
+}
