85450ff85e8ff71c25c55ca124b06cde40c556c8
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-09 21:21:32 +0700

    footer v1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-black flex justify-center text-white py-4'>Copyright © 2024 Determinix | Design by Power</div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../css/output.css"
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function home() {
   return (
