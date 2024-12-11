295e2eb7e432734dd80188f7c258eb44170466ec
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-11 21:53:16 +0700

    title

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Divider from '../elements/Divider'
+import Title from '../elements/Title'
 import More from '../elements/More'
 
 export default function Showcase() {
   return (
     <div>
       <Divider/>
+      <Title what={"Showcase"}/>
       <More what={"Showcases"} where={"showcase"}/>
     </div>
   )
---
+++ src/elements/Title.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Title({what}) {
+  return (
+    <div className='text-center text-xl font-medium pt-4'>{what}</div>
+  )
+}
