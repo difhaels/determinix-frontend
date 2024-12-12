f8c5d2f6d70280ea606ef411356fd2f42060be00
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-12 21:09:47 +0700

    activities

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Divider from '../elements/Divider'
+import Title from '../elements/Title'
+import CardActivities from '../elements/CardActivities'
+import More from '../elements/More'
 
 export default function Activities() {
   return (
-    <div>Activities</div>
+    <div>
+      <Divider/>
+      <Title what={"Activities"}/>
+      <div className='flex justify-center py-5'>
+        <CardActivities />
+        <CardActivities />
+        <CardActivities />
+      </div>
+      <More what={"Activities"} where={"Activities"}/>
+    </div>
   )
 }
