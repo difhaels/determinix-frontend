a8f9c03e61117a5879ccbc495f8d9123fee9c0a6
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-12 21:04:22 +0700

    articles

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Divider from '../elements/Divider'
+import Title from '../elements/Title'
+import CardArticles from '../elements/CardArticles'
+import More from '../elements/More'
 
 export default function Articles() {
   return (
-    <div>Articles</div>
+    <div>
+      <Divider/>
+      <Title what={"Articles"}/>
+      <div className='flex justify-center py-5'>
+        <CardArticles />
+        <CardArticles />
+        <CardArticles />
+      </div>
+      <More what={"Articles"} where={"articles"}/>
+    </div>
   )
 }
---
+++ src/elements/CardArticles.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import lepy from "../assets/lepy.png";
+
+export default function CardShowCase() {
+  return (
+    <div className="px-2 py-2">
+      <div className="w-64 h-32">
+        <img
+          src={lepy}
+          alt="a"
+          className="w-full h-full "
+        />
+      </div>
+      <div className="w-64 px-2">
+        <h1 className="text-base underline text-slate-600">GENERAL</h1>
+        <h1 className="text-sm text-slate-600">June 19, 2024</h1>
+        <h1 className="text-base font-medium">Articles 01</h1>
+        <h1 className="text-sm text-slate-600">Agung Saputra</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?</h1>
+        <a href="#" className="text-sm text-sky-500">Read More</a>
+      </div>
+    </div>
+  );
+}
