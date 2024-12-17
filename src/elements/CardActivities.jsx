bb8ab560f7e7d2d250e9ebd9330d8608ff3cc91e
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-17 22:30:05 +0700

    update constans

--- src/elements/CardActivities.jsx
+++ src/elements/CardActivities.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import lepy from "../assets/lepy.png";
 
-export default function CardActivities({full}) {
+export default function CardActivities({ full }) {
   return (
-    <div className={`bg-white px-2 py-2 ${full?"p-4 shadow-lg flex":""}`}>
+    <div className={`bg-white px-2 py-2 ${full ? "p-4 shadow-lg flex" : ""}`}>
       <div className="w-64 h-44 overflow-hidden">
-        <img
-          src={lepy}
-          alt="a"
-          className="w-full h-full object-cover "
-        />
+        <img src={lepy} alt="a" className="w-full h-full object-cover " />
       </div>
       <div className="w-64 px-2">
         <h1 className="text-sm text-slate-600">June 19, 2024</h1>
         <h1 className="text-base font-medium">Activities 01</h1>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?</h1>
-        <a href="yt.com" className="text-sm text-sky-500">Read More</a>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem a voluptas, dicta ex reprehenderit ullam?
+        </h1>
+        <div className="hover:scale-105 hover:px-2">
+          <a href="yt.com" className="text-sm text-sky-500">
+            Read More
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/test/constant.js
+++ src/test/constant.js
@@ -35,3 +35,77 @@ export const projects = [
     img: "/assets/lepy.png"
   },
 ];
+
+export const activities = [
+  {
+    id: 8001,
+    date: 'June 18, 2023',
+    title: 'Activities 01',
+    img:"/assets/lepy.png"
+  },
+  {
+    id: 8002,
+    date: 'June 18, 2023',
+    title: 'Activities 02',
+    img:"/assets/lepy.png"
+  },
+  {
+    id: 8003,
+    date: 'June 18, 2023',
+    title: 'Activities 03',
+    img:"/assets/lepy.png"
+  },
+  {
+    id: 8004,
+    date: 'June 18, 2023',
+    title: 'Activities 04',
+    img:"/assets/lepy.png"
+  },
+  {
+    id: 8005,
+    date: 'June 18, 2023',
+    title: 'Activities 05',
+    img:"/assets/lepy.png"
+  },
+  {
+    id: 8006,
+    date: 'June 18, 2023',
+    title: 'Activities 06',
+    img:"/assets/lepy.png"
+  },
+]
+
+export const articles = [
+  {
+    id: 800001,
+    date: 'May 18, 2023',
+    title: 'Articles 01',
+    type: 'GENERAL',
+    short: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?',
+    img:"/assets/lepy.png"
+  },
+  {
+    id: 800002,
+    date: 'May 18, 2023',
+    title: 'Articles 02',
+    type: 'GENERAL',
+    short: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?',
+    img:"/assets/lepy.png"
+  },
+  {
+    id: 800002,
+    date: 'May 18, 2023',
+    title: 'Articles 03',
+    type: 'GENERAL',
+    short: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?',
+    img:"/assets/lepy.png"
+  },
+  {
+    id: 800004,
+    date: 'May 18, 2023',
+    title: 'Articles 04',
+    type: 'GENERAL',
+    short: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?',
+    img:"/assets/lepy.png"
+  },
+]
