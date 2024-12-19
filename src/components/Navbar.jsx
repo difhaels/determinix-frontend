64a2a75c33aabf24daf30752701c8eee1aaaf110
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-19 23:16:18 +0700

    navbar responsive v1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,12 +13,12 @@ export default function Navbar({ isScrolled }) {
   ];
 
   return (
-    <nav className={`top-0 h-[4rem] w-full flex justify-between fixed z-[2] py-0 px-16 items-center ${isScrolled ? "bg-white  shadow-sm" : ""}`}>
-      <div className="flex h-10">
+    <nav className={`top-0 h-[4rem] w-full flex justify-between fixed z-[2] py-0 px-4 lg:px-16 items-center ${isScrolled ? "bg-white  shadow-sm" : ""}`}>
+      <div className="flex h-8 lg:h-10">
         <img src={dxlogo} alt="dxlogo"/>
       </div>
       <div>
-        <ul className="flex justify-between items-center gap-20">
+        <ul className="lg:flex justify-between items-center gap-20 hidden">
           {links.map(({ name, link }) => {
             return (
               <li key={name} className="hover:text-red-700 hover:scale-105">
