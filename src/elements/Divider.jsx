0da0aa4a718e3bedc04d85f88f77578bc12ea8a6
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-19 23:24:54 +0700

    responsive divider

--- src/elements/Divider.jsx
+++ src/elements/Divider.jsx
@@ -3,7 +3,7 @@ import dx from "../assets/dx-logo.png";
 
 export default function Divider() {
   return (
-    <div className="flex items-center my-4 mx-16">
+    <div className="flex items-center my-4 mx-8 lg:mx-16">
       <div className="flex-grow border-t border-gray-400"></div>
       <img src={dx} alt="dx" className="w-5 mx-2"/>
       <div className="flex-grow border-t border-gray-400"></div>
