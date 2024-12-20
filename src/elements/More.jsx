dd80c2df053924da34896d47d7956cf19e6a193c
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-20 09:50:08 +0700

    responsive show more

--- src/elements/More.jsx
+++ src/elements/More.jsx
@@ -5,9 +5,9 @@ import { ArrowRightIcon } from "@heroicons/react/24/solid";
 export default function More({what, where}) {
   return (
     <div className='flex justify-center'>
-      <Link to={where} className='px-4 py-2 bg-red-500 hover:bg-red-400 rounded-md text-white flex'>
+      <Link to={where} className='lg:px-4 lg:py-2 py-1 px-2 bg-red-500 hover:bg-red-400 rounded-md text-white flex items-center lg:text-base text-sm'>
         <h1>See more {what} </h1>
-        <ArrowRightIcon className="w-6 h-6 pl-1" />
+        <ArrowRightIcon className="w-5 h-5 lg:w-6 lg:h-6  pl-1" />
       </Link>
     </div>
   )
