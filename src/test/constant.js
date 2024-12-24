69425322e27fc8542f388d094c4290b55201d7ee
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-24 23:38:36 +0700

    new relasi v0.02

--- src/test/constant.js
+++ src/test/constant.js
@@ -3,142 +3,84 @@ export const projects = [
     id: 81,
     title: "Project 1",
     date: "22-08-2023",
-    members: ["agung", "albi", "aru"],
-    img: "/assets/lepy.png"
+    members: [8818, 2, 3], // Refer to members by ID
+    img: "/assets/lepy.png",
   },
   {
     id: 82,
     title: "Project 2",
     date: "22-08-2023",
-    members: ["agung", "wisnu", "beni", "chandra"],
-    img: "/assets/lepy.png"
+    members: [8818, 8, 4, 5],
+    img: "/assets/lepy.png",
   },
   {
     id: 83,
     title: "Project 3",
     date: "22-08-2023",
-    members: ["agung", "dian", "chandra", "krisna"],
-    img: "/assets/lepy.png"
+    members: [8818, 7, 4, 6],
+    img: "/assets/lepy.png",
   },
   {
     id: 84,
     title: "Project 4",
     date: "22-08-2023",
-    members: ["albi", "dian", "chandra", "wisnu"],
-    img: "/assets/lepy.png"
+    members: [2, 7, 4, 8],
+    img: "/assets/lepy.png",
   },
   {
     id: 85,
     title: "Project 5",
     date: "22-08-2023",
-    members: ["agung", "aru", "chandra", "krisna"],
-    img: "/assets/lepy.png"
+    members: [8818, 3, 4, 6],
+    img: "/assets/lepy.png",
   },
 ];
 
 export const activities = [
   {
     id: 8001,
-    date: 'June 18, 2023',
-    title: 'Activities 01',
-    img:"/assets/lepy.png"
+    date: "June 18, 2023",
+    title: "Activities 01",
+    img: "/assets/lepy.png",
   },
   {
     id: 8002,
-    date: 'June 18, 2023',
-    title: 'Activities 02',
-    img:"/assets/lepy.png"
+    date: "June 18, 2023",
+    title: "Activities 02",
+    img: "/assets/lepy.png",
   },
-  {
-    id: 8003,
-    date: 'June 18, 2023',
-    title: 'Activities 03',
-    img:"/assets/lepy.png"
-  },
-  {
-    id: 8004,
-    date: 'June 18, 2023',
-    title: 'Activities 04',
-    img:"/assets/lepy.png"
-  },
-  {
-    id: 8005,
-    date: 'June 18, 2023',
-    title: 'Activities 05',
-    img:"/assets/lepy.png"
-  },
-  {
-    id: 8006,
-    date: 'June 18, 2023',
-    title: 'Activities 06',
-    img:"/assets/lepy.png"
-  },
-]
+];
 
 export const articles = [
   {
     id: 800001,
-    writer: 'Agung Saputra',
-    date: 'May 18, 2023',
-    title: 'Articles 01',
-    type: 'GENERAL',
-    short: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?',
-    img:"/assets/lepy.png"
+    writerId: 8818, // Reference to member ID
+    date: "May 18, 2023",
+    title: "Articles 01",
+    type: "GENERAL",
+    short:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?",
+    img: "/assets/lepy.png",
   },
   {
     id: 800019,
-    writer: 'Wisnu H',
-    date: 'May 18, 2023',
-    title: 'Articles 02',
-    type: 'GENERAL',
-    short: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?',
-    img:"/assets/lepy.png"
+    writerId: 8, // Reference to member ID
+    date: "May 18, 2023",
+    title: "Articles 02",
+    type: "GENERAL",
+    short:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?",
+    img: "/assets/lepy.png",
   },
-  {
-    id: 800002,
-    writer: 'Aru C',
-    date: 'May 18, 2023',
-    title: 'Articles 03',
-    type: 'GENERAL',
-    short: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?',
-    img:"/assets/lepy.png"
-  },
-  {
-    id: 800004,
-    writer: 'Salsabil A',
-    date: 'May 18, 2023',
-    title: 'Articles 04',
-    type: 'GENERAL',
-    short: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?',
-    img:"/assets/lepy.png"
-  },
-  {
-    id: 800005,
-    writer: 'Beni',
-    date: 'May 18, 2023',
-    title: 'Articles 04',
-    type: 'GENERAL',
-    short: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?',
-    img:"/assets/lepy.png"
-  },
-  {
-    id: 800006,
-    writer: 'Tubagus',
-    date: 'May 18, 2023',
-    title: 'Articles 05',
-    type: 'GENERAL',
-    short: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem a voluptas, dicta ex reprehenderit ullam?',
-    img:"/assets/lepy.png"
-  },
-]
+];
 
 export const members = [
-    {id: 8818, name: "Agung Saputra", j:"The Strongest", img: "/assets/agung.jpeg", ig:"difhaels"},
-    {id: 2, name: "A** C** R**", j:"Not Set", img: 0, },
-    {id: 3, name: "B** M**", j:"Not Set", img: 0},
-    {id: 4, name: "C** H** R**", j:"Not Set", img: 0},
-    {id: 5, name: "D** R**", j:"Not Set", img: 0},
-    {id: 6, name: "K** S** R**", j:"Not Set", img: 0},
-    {id: 7, name: "S** A** F**", j:"Not Set", img: 0},
-    {id: 8, name: "W** H**", j:"Not Set", img: 0},
-  ]
+  { id: 8818, name: "Agung Saputra", j: "The Strongest", img: "/assets/agung.jpeg", ig: "difhaels" },
+  { id: 2, name: "A** C** R**", j: "Not Set", img: 0 },
+  { id: 3, name: "B** M**", j: "Not Set", img: 0 },
+  { id: 4, name: "C** H** R**", j: "Not Set", img: 0 },
+  { id: 5, name: "D** R**", j: "Not Set", img: 0 },
+  { id: 6, name: "K** S** R**", j: "Not Set", img: 0 },
+  { id: 7, name: "S** A** F**", j: "Not Set", img: 0 },
+  { id: 8, name: "W** H**", j: "Not Set", img: 0 },
+];
