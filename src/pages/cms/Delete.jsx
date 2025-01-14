9f8a1fe5e75cf0314a52336cb8399db93a69ab57
Agung Saputra <agungsaputradifh@gmail.com>
2025-01-14 07:44:08 +0700

    delete confirm

--- src/pages/cms/Delete.jsx
+++ src/pages/cms/Delete.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import error from "../../assets/error.png";
 
 export default function Ups({ type, idOf, nameOf, close }) {
+  const [confirm, setConfirm] = useState("");
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white rounded shadow-lg max-w-md ">
@@ -21,9 +23,24 @@ export default function Ups({ type, idOf, nameOf, close }) {
           <h1 className="text-sm">{idOf}</h1>
         </div>
         <div className="px-5 pb-5 pt-2">
-          <p className="mb-1">To confirm, type "dx/{nameOf}" in the box below</p>
-          <input type="text" className="mb-2 border-2 border-red-500 rounded-sm w-full px-2"/>
-          <button className="flex items-center justify-center bg-slate-300 text-red-500 w-full rounded-sm">Delete this {type}</button>
+          <p className="mb-1">
+            To confirm, type "dx/{nameOf}" in the box below
+          </p>
+          <input
+            type="text"
+            className="mb-2 border-2 border-red-500 rounded-sm w-full px-2"
+            value={confirm}
+            onChange={(e) => setConfirm(e.target.value)}
+          />
+          {confirm === "dx/"+nameOf ? (
+            <button className="flex items-center justify-center bg-red-500 text-white w-full rounded-sm py-1">
+              Delete this {type}
+            </button>
+          ) : (
+            <button className="flex items-center justify-center bg-slate-300 text-red-500 w-full rounded-sm py-1">
+              Delete this {type}
+            </button>
+          )}
         </div>
       </div>
     </div>
