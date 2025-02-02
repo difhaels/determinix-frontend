37fd658b8c003c4c9571d60cec5cad0dd4f8ca8b
Agung Saputra <agungsaputradifh@gmail.com>
2025-02-02 19:07:10 +0700

    update nothinh

--- src/components/Nothing.jsx
+++ src/components/Nothing.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 import error from "../assets/error.png";
+import { Link } from "react-router-dom";
 
-export default function Nothing({what, desc}) {
+export default function Nothing({ what, desc }) {
   return (
     <div className="flex items-center gap-3 justify-center pt-12">
       <img src={error} alt="" className="w-20" />
       <div>
         <h1 className="font-semibold text-lg">{what}</h1>
         <h1>{desc}</h1>
+        <div className="pt-2">
+          <Link to={"/"} className="bg-sky-500 text-white p-1 rounded-sm">Refresh</Link>
+        </div>
       </div>
     </div>
   );
